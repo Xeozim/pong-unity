6c976afa679b9f2243d297152ca48c2c5f2e0cf3
Xeozim <5320152+Xeozim@users.noreply.github.com>
2024-10-05 12:28:15 +0100

    Simple opponent AI, good enough for the current speeds

---
+++ Assets/Scripts/Pong/PongAI.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PongAI : MonoBehaviour
+{
+    [SerializeField] private PongSettings settings;
+
+    private PongBall ball;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<PongBall>();
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the ball is moving away from us, move towards the centre of the screen
+        if (ball.Velocity.x < 0)
+        {
+            targetPosition.y = 0;
+        } else {
+            // Predict the y impact of the ball and move towards that
+            // Time until the ball reaches our x position (assumes we only move in y and that
+            // the ball won't lose energy en route)  
+            var timeToIntercept = (transform.position.x - ball.transform.position.x) / ball.Velocity.x;
+
+            targetPosition.y = ball.transform.position.y + (ball.Velocity.y * timeToIntercept);
+        }
+
+        // Set velocity to achieve the target position, very simple proportional control by clamping
+        // Multiplying position offset by 2 means we use full speed unless the target position is
+        // within paddleSpeed / 2 of the current. Really we should use the actual dimensions of the
+        // paddle to control this because this behaviour will change with paddle speed.
+        var velocity = Mathf.Clamp((targetPosition.y - transform.position.y) * 2, -settings.paddleSpeed, settings.paddleSpeed);
+
+        // Update position, clamping to game limits
+        var newYPosition = Mathf.Clamp(transform.position.y + velocity * Time.deltaTime, settings.yMinimum, settings.yMaxmium);
+        transform.position = new Vector3(transform.position.x,newYPosition,transform.position.z);
+    }
+}
